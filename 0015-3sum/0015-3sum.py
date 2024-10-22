9352a537530f23c44f777a8102798102eeceeefb
SEON MI KIM <dev.ddubbu@gmail.com>
2024-10-22 17:05:22 +0900

    Time: 598 ms (81.98%), Space: 19 MB (99.97%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -2,14 +2,21 @@
 two pointer
 - 정렬 필수!
 - learning target: sum of group is 0
+
+중복값 처리
+- tuple, set 이용하기
+- 사전에 없애기
 """
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
-        result = set()
+        result = []
 
         size = len(nums)
         for i in range(size-2):
+            if i>0 and nums[i] == nums[i-1]: # 중복 제거
+                continue
+
             left = i + 1
             right = size - 1
 
@@ -21,13 +28,19 @@ class Solution:
                 elif total > 0:
                     right -= 1
                 else: # total == 0
-                    tmp = (nums[i], nums[left], nums[right])
-                    result.add(tmp)
+                    tmp = [nums[i], nums[left], nums[right]]
+                    result.append(tmp)
 
                     left += 1
                     right -= 1
 
-        return list(map(list, result))
+                    # 중복 제거 (각각해야, while 탈출 조건 부합)
+                    while left < right and nums[left] == nums[left-1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right+1]:
+                        right -= 1
+                    
+        return result
         
 
 
