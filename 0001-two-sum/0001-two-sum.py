9df2b8e71fef0f01150e5f7e5d878bd386a4b34e
SEON MI KIM <dev.ddubbu@gmail.com>
2024-10-22 14:50:34 +0900

    Time: 1663 ms (28.29%), Space: 17.3 MB (93.05%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,16 +1,8 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-
-        size = len(nums)
-        for i in range(size-1):
-            for j in range(i+1, size):
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        length = len(nums)
+        for i in range(length):
+            for j in range(i+1, length):
                 if nums[i] + nums[j] == target:
                     return [i, j]
-                    break
-
         
