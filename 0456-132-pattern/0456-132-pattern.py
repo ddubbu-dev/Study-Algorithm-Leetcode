85fc9161b31265a8ebfe65642dfa37cb576a1811
SEON MI KIM <dev.ddubbu@gmail.com>
2024-10-22 22:38:21 +0900

    Time: 83 ms (98.09%), Space: 34.4 MB (97.21%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.py
+++ 0456-132-pattern/0456-132-pattern.py
@@ -22,7 +22,7 @@ class Solution:
             # print("n_j", n_j)
             # print("stack", stack)
             while stack and stack[-1] < n_j:  # Q. 왜 while문 돌아야하지?
-                n_k = stack[-1] # prev n_j -> n_k
+                n_k = max(stack[-1], n_k) # prev n_j -> n_k
                 stack.pop()
             
             stack.append(n_j) # n_j보다 같거나 큰 값들만 존재
